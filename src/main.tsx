ae1d6ff35a43243d0f4060e3232b61cb9aafe5e4
Zeka5 <zekovicognjen753@gmail.com>
2026-08-15 21:57:35 +0200

    initial router fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
 
 const Router = import.meta.env.VITE_USE_HASH_ROUTER === 'true' ? HashRouter : BrowserRouter;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router basename={import.meta.env.VITE_USE_HASH_ROUTER === 'true' ? undefined : '/prostor/'}>
+      <Routes>
+        <Route path="/" element={<h1>Prostor</h1>} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
