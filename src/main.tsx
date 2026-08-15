3ff05403a01b19f35c74e936fdba693d0f18bd1b
Zeka5 <zekovicognjen753@gmail.com>
2026-08-15 21:53:07 +0200

    pages deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+
+const Router = import.meta.env.VITE_USE_HASH_ROUTER === 'true' ? HashRouter : BrowserRouter;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
